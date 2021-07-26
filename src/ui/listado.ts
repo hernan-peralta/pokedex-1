4dad1a86d4a218c166b8239158fecc8ddf4c1d82
Hernán Peralta <alegodemencia@gmail.com>
2021-07-26 01:34:16 -0300

    Cambia ui/listado.js a .ts

---
+++ src/ui/listado.ts
@@ -0,0 +1,18 @@
+export function actualizarTextoIndicePokemones(texto: string) {
+  const $indice = <HTMLDivElement>document.querySelector('#indice');
+  $indice.textContent = texto;
+}
+
+export function mostrarListadoPokemones(nombresPokemones: string[], pokemonSeleccionadoCallback = (nombre: string) => {}) {
+  const $indice = <HTMLDivElement>document.querySelector('#indice');
+  $indice.innerHTML = '';
+
+  nombresPokemones.forEach((nombre) => {
+    const $link = document.createElement('a');
+    $link.className = 'list-group-item list-group-item-action';
+    $link.setAttribute('href', '#');
+    $link.textContent = nombre;
+    $link.onclick = () => pokemonSeleccionadoCallback(nombre);
+    $indice.appendChild($link);
+  });
+}
